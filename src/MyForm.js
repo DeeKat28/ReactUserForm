6cbfa81993e71503c54ed77ad3590b54bd6601d5
DeeKat <deekat.devVerse@gmail.com>
2020-04-28 12:59:04 +0530

    implemented state and form submit

--- src/MyForm.js
+++ src/MyForm.js
@@ -3,10 +3,40 @@ import './MyForm.css';
 
 class MyForm extends React.Component {
 
+    // constructor(props) {
+    //     super(props);
+
+    //     this.onHandleSubmit = this.onHandleSubmit.bind(this);
+    // }
+
+    state = {
+        age: null,
+        eligible: 'No',
+        eligibleBackgroundColor: 'red'
+    }
+
+    onHandleSubmit = (event) => {
+        event.preventDefault();
+        
+        this.setState({ age: document.getElementById('age').value });
+        
+        const userAge = Number.parseInt(document.getElementById('age').value);
+
+        if (userAge > 18) {
+            this.setState({ eligible: 'Yes' });
+            this.setState({ eligibleBackgroundColor: 'green' });
+        }
+        else {
+            this.setState({ eligible: 'No' });
+            this.setState({ eligibleBackgroundColor: 'red' })
+        }
+
+    }
+
     render() {
         return (
             <div className="form-content">
-                <form className="form-container" action="#" method="POST">
+                <form className="form-container" action="#" method="POST" onSubmit={this.onHandleSubmit}>
                     <label htmlFor="name">Name: </label>
                     <input 
                         id="name" 
@@ -51,11 +81,12 @@ class MyForm extends React.Component {
                     <br />
                     <label htmlFor="eligiblity">Eligible for Voting: </label>
                     <input
+                        style={{ background: this.state.eligibleBackgroundColor, color: '#fff' }}
                         id="eligiblity"
                         name="eligiblity"
                         type="text"
-                        value="No"
-                        disabled="true"
+                        value={this.state.eligible}
+                        readOnly={true}
                     />
                     {/* <input 
                         className="radioBtn"
